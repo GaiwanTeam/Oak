8bed89fc0282125f01e4d5c56907e6bd74ec8a44
Arne Brasseur <arne@arnebrasseur.net>
2025-08-10 12:03:12 +0200

    Fresh from template

---
+++ src/co/gaiwan/locker/config.clj
@@ -0,0 +1,32 @@
+(ns co.gaiwan.locker.config
+  (:refer-clojure :exclude [get])
+  (:require
+   [lambdaisland.config :as config]
+   [lambdaisland.config.cli :as config-cli]
+   [lambdaisland.makina.app :as app]))
+
+(def prefix "locker")
+
+(defonce cli-opts (atom {}))
+
+(defonce config
+  (config-cli/add-provider
+   (config/create {:prefix prefix})
+   cli-opts))
+
+(def get (partial config/get config))
+(def source (partial config/source config))
+(def sources (partial config/sources config))
+(def entries (partial config/entries config))
+(def reload! (partial config/reload! config))
+
+(defonce system
+  (app/create
+   {:prefix prefix
+    :data-readers {'config get}}))
+
+(def load! (partial app/load! system))
+(def start! (partial app/start! system))
+(def stop! (partial app/stop! system))
+(def refresh (partial app/refresh `system))
+(def refresh-all (partial app/refresh-all `system))
