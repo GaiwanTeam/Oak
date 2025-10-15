1d2596a5def20f4b42c59f56b8bb527ba4569d4e
Laurence Chen <laurence@replware.dev>
2025-10-15 18:05:20 +0800

    - fix a bug inside wrap-session-auth - add middleware: `wrap-enforce-login` - rediect to login when user access totp/setup, totp/verify - redirect back to totp/* after login

--- src/co/gaiwan/oak/lib/auth_middleware.clj
+++ src/co/gaiwan/oak/lib/auth_middleware.clj
@@ -12,7 +12,27 @@
    [co.gaiwan.oak.domain.identity :as identity]
    [co.gaiwan.oak.domain.jwt :as jwt]
    [co.gaiwan.oak.domain.scope :as scope]
+   [co.gaiwan.oak.util.routing :as routing]
    [ring.util.response :as res]))
+
+(defn authenticated?
+  "make sure the user is logged in"
+  [req]
+  (some? (:identity req)))
+
+(defn wrap-enforce-login
+  "Middleware that redirects to login page if the user is not logged in"
+  [handler]
+  (fn [req]
+    (if (authenticated? req)
+      (handler req)
+      {:status 302
+       :headers {"Location" (routing/url-for req :auth/login)}
+       :session {:redirect-after-login (:uri req)}})))
+
+(comment
+  (def h (wrap-enforce-login (constantly 1)))
+  (h {}))
 
 (defn wrap-session-auth
   "If an `:identity` key is set in the session, this will look up the user with
@@ -25,7 +45,7 @@
           [scheme token] (when auth-header (str/split auth-header #"\s+"))]
       (h
        (if-let [id (and (:identity session)
-                        (identity/find-one db (:identity session)))]
+                        (identity/find-one db {:id (:identity session)}))]
          (assoc req
                 :identity id
                 :auth-time (:auth-time session))
