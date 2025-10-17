292ac981a5347a4e332bd7daef330b26e3b8add2
Laurence Chen <laurence@replware.dev>
2025-10-17 18:00:41 +0800

    change totp/setup page

--- src/co/gaiwan/oak/html/totp.clj
+++ src/co/gaiwan/oak/html/totp.clj
@@ -4,11 +4,6 @@
    [co.gaiwan.oak.lib.form :as form]
    [lambdaisland.ornament :as o]))
 
-(defn setup-page [{:keys [data-uri next-uri]}]
-  [:p "Set up TOTP here!"
-   [:img {:src data-uri}]
-   [:a {:href next-uri} "Continue"]])
-
 (defn verify-failed-page [{:keys [next-uri]}]
   [:div "Invalid code. Please check and re-enter."
    [:a {:href next-uri} "Continue"]])
@@ -17,6 +12,56 @@
   (if cred-save-success?
     [:div "Your authenticator device has been successfully linked."]
     [:div "Encountering error when recording credentials"]))
+
+(comment
+  (defn setup-page [{:keys [data-uri next-uri]}]
+    [:p "Set up TOTP here!"
+     [:img {:src data-uri}]
+     [:a {:href next-uri} "Continue"]]))
+
+(o/defstyled next-link :a.call-to-action
+  {:width            "100%"
+   :padding          --size-2
+   :border           :none
+   :border-radius    --radius-2
+   :font-size        "1rem"
+   :font-weight      :bold
+   :cursor           :pointer})
+
+(o/defstyled next-button :input.call-to-action
+  {:width            "100%"
+   :padding          --size-2
+   :border           :none
+   :border-radius    --radius-2
+   :font-size        "1rem"
+   :font-weight      :bold
+   :cursor           :pointer})
+
+(o/defstyled setup-page :main
+  {:display         :flex
+   :justify-content :center
+   :align-items     :center
+   :min-height      "100vh"}
+  [:>div
+   {:margin    "0 auto"
+    :padding   --size-8
+    :flex-grow 1}]
+  [:at-media {:min-width "40rem"}
+   [:>div
+    {:display          :flex
+     :flex-direction   :column
+     :max-width        --size-fluid-10
+     :flex-grow        1
+     :border-radius    --radius-3
+     :box-shadow       --shadow-2
+     :background-color --bg-panel
+     :color            --text-panel}]]
+  ([{:keys [data-uri next-uri]}]
+   [:div
+    [:p "Set up TOTP here!"]
+    [:img {:src data-uri}]
+    [next-link
+     {:href next-uri} "Continue"]]))
 
 (comment
   (defn verify-form []
@@ -36,15 +81,6 @@
    [:<>
     [:label {:for (:id props)} (:label props)]
     [:input props]]))
-
-(o/defstyled submit :input.call-to-action
-  {:width            "100%"
-   :padding          --size-2
-   :border           :none
-   :border-radius    --radius-2
-   :font-size        "1rem"
-   :font-weight      :bold
-   :cursor           :pointer})
 
 (o/defstyled verify-form :main
   {:display         :flex
@@ -76,4 +112,4 @@
         :name             "code"
         :required         "required"
         :aria-describedby "2FA code"}]
-      [submit {:type "submit" :value "Verify 2FA Setup"}]]]]))
+      [next-button {:type "submit" :value "Verify 2FA Setup"}]]]]))
