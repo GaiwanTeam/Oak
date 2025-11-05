2962e4e876db1c8ed9f8a0fa0af852f96dd1ee12
Laurence Chen <laurence@replware.dev>
2025-11-05 16:48:34 +0800

    make the three columns equal size wide

--- src/co/gaiwan/oak/html/dashboard.clj
+++ src/co/gaiwan/oak/html/dashboard.clj
@@ -15,22 +15,21 @@
 
 (o/defstyled card-container :div
   {:display :grid
-   :grid-template-columns "repeat( auto-fit, minmax(40em, 1fr) )"
-   :marging "1rem"
-   :gap "1rem"})
+   :grid-template-columns "repeat( auto-fit, minmax(20rem, 1fr) )"
+   :gap "1.5rem"})
 
 (defn dash-page [{:keys [req debug? totp-setup-url logout-url]}]
   [dash-layout
    [card-container
-    [w/card
+    [w/column-card
      [:h1 "Account Security"]
      [:a {:href totp-setup-url} "Setup 2FA"]
      [:a {:href logout-url} "Sign out"]]
-    [w/card
+    [w/column-card
      [:h1 "Change Password"]]
-    [w/card
-     [:h1 "Authentications"]]
+    [w/column-card
+     [:h1 "Authorized Applications"]]
     (when debug?
-      [w/card
+      [w/column-card
        [:h1 "Debug"]
        [:p (pr-str (:identity req))]])]])
