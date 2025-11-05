4c4278fca318f7ca55679afadf93b8c0fc9bacec
Laurence Chen <laurence@replware.dev>
2025-11-05 18:23:54 +0800

    add avatar && color

--- src/co/gaiwan/oak/html/dashboard.clj
+++ src/co/gaiwan/oak/html/dashboard.clj
@@ -7,6 +7,9 @@
    [lambdaisland.ornament :as o]))
 
 (o/defstyled dash-layout :div
+  {:display :flex
+   :flex-direction :column
+   :padding "2rem"}
   ([& children]
    (into
     [:<>
@@ -18,17 +21,37 @@
    :grid-template-columns "repeat( auto-fit, minmax(20rem, 1fr) )"
    :gap "1.5rem"})
 
+(o/defstyled dash-header :div
+  {:margin-bottom "2rem"}
+  [:>div {:display :flex
+          :align-items :center
+          :gap "1rem"}]
+  [:.user-details {:margin 0
+                   :padding 0}]
+  [:h2 {:margin-top 0
+        :margin-bottom "0.25rem"
+        :padding 0}]
+  [:p {:color --gray-8
+       :margin 0
+       :padding 0}])
+
 (defn dash-page [{:keys [req debug? totp-setup-url logout-url]}]
   [dash-layout
+   [dash-header
+    [:div.user-info
+     [w/avatar]
+     [:div.user-details
+      [:h2 "John Doe"]
+      [:p "john.doe@example.com"]]]]
    [card-container
     [w/column-card
-     [:h1 "Account Security"]
+     [:h3 "Account Security"]
      [:a {:href totp-setup-url} "Setup 2FA"]
      [:a {:href logout-url} "Sign out"]]
     [w/column-card
-     [:h1 "Change Password"]]
+     [:h3 "Change Password"]]
     [w/column-card
-     [:h1 "Authorized Applications"]]
+     [:h3 "Authorized Applications"]]
     (when debug?
       [w/column-card
        [:h1 "Debug"]
